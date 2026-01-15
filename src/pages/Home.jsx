9a62ec6b4ab7ab71b0d0259f2796688d3198c3bc
NafisRohan12 <nafisislamrohan@gmail.com>
2026-01-15 19:08:18 +0600

    Final work

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -27,9 +27,24 @@ function Home() {
   }, []); /*control it with the dependency array = inside of this if anything of 
   those values change we will run the effect or if nothing 
   there then run one time . right when compenent in rendered*/
-  const handleSearch = (e) => {
+
+  const handleSearch = async (e) => {
     e.preventDefault();
-    alert(searchQuery);
+
+    if (!searchQuery.trim()) return;
+    if (loading) return;
+
+    setLoading(true);
+    try {
+      const searchResults = await searchMovies(searchQuery);
+      setMovies(searchResults);
+      setError(null);
+    } catch (err) {
+      console.log(err);
+      setError("Failed to search movies...");
+    } finally {
+      setLoading(false);
+    }
   };
 
   return (
--- src/services/api.js
+++ src/services/api.js
@@ -1,12 +1,12 @@
-const API_KEY = "8c13dc00dced5f1e900d7f90056b44f4"
-const BASE_URL = "https://api.themoviedb.org/3"
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+const BASE_URL = "https://api.themoviedb.org/3";
 
 export const getPopularMovies = async () => {
     const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
     const data = await response.json();
     return data.results
 };
-export const searchMovies = async () => {
+export const searchMovies = async (query) => {
     const response = await fetch(`${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(query)}`);
     const data = await response.json();
     return data.results
